f8ffbad5cd35b78246d3e34dcb1fe731763b8ecc
hacker1998-thosiba <sowcheikh086@gmail.com>
2022-03-30 23:16:10 +0000

    Gestion de l authentification et du design Tasnim

---
+++ resources/views/components/design_tasnim/footer.blade.php
@@ -0,0 +1,30 @@
+</main>
+
+<footer id="footer">
+  <div class="container">
+    <div class="copyright">
+      &copy; Copyright <strong><span>Tasnim Immo</span></strong>
+    </div>
+    <div class="credits">
+      <a href="">EpsilonCoder</a>
+    </div>
+  </div>
+</footer>
+
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+
+<script src="tasnim/vendor/purecounter/purecounter.js"></script>
+<script src="tasnim/vendor/aos/aos.js"></script>
+<script src="tasnim/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="tasnim/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="tasnim/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="tasnim/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="tasnim/vendor/typed.js/typed.min.js"></script>
+<script src="tasnim/vendor/waypoints/noframework.waypoints.js"></script>
+<script src="tasnim/vendor/php-email-form/validate.js"></script>
+<script src="tasnim/js/main.js"></script>
+
+</body>
+
+</html>
